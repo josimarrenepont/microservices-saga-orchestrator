646f86200912d88e48a0386f9c870ab99be156dc
josimarrenepont <jnrenepont@hotmail.com>
2025-12-13 21:14:22 -0300

    Created Inventory and OrderInventory class

---
+++ inventory-service/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.inventory (id, product_code, avaliable) VALUES (1, 'BOOKS', 3);
+INSERT INTO public.inventory (id, product_code, avaliable) VALUES (2, 'MUSIC', 7);
+INSERT INTO public.inventory (id, product_code, avaliable) VALUES (3, 'MOVIES', 10);

58f1bcdd169f5adf01c6a4e02a3637945e2b3843
josimarrenepont <jnrenepont@hotmail.com>
2025-12-17 22:26:07 -0300

    Added SagaHandler class for order orchestration

--- inventory-service/src/main/resources/import.sql
+++ inventory-service/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO public.inventory (id, product_code, avaliable) VALUES (1, 'BOOKS', 3);
-INSERT INTO public.inventory (id, product_code, avaliable) VALUES (2, 'MUSIC', 7);
-INSERT INTO public.inventory (id, product_code, avaliable) VALUES (3, 'MOVIES', 10);
+INSERT INTO public.inventory (id, product_code, available) VALUES (1, 'BOOKS', 3);
+INSERT INTO public.inventory (id, product_code, available) VALUES (2, 'MUSIC', 7);
+INSERT INTO public.inventory (id, product_code, available) VALUES (3, 'MOVIES', 10);
